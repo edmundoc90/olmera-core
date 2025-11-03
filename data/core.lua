71987550f7f88b77a35150e73336229bfcb084a5
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 13:24:57 -0600

    chore: remove test marker from core.lua

--- data/core.lua
+++ data/core.lua
@@ -1,7 +1,6 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
--- Pipeline test: clean folders before tar extraction
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
