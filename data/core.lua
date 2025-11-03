5591bf6fd26d5edee97c9069694b63de31346c52
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 13:08:09 -0600

    test: verify tar+ssh extracts to correct location

--- data/core.lua
+++ data/core.lua
@@ -1,7 +1,7 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
--- Pipeline test: rsync to avoid nested dirs
+-- Pipeline test: tar+ssh extraction in place
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
