5b677a55cf7dd8250de89e7d36bae8de518b95a6
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 13:05:52 -0600

    test: verify rsync uploads to correct path

--- data/core.lua
+++ data/core.lua
@@ -1,7 +1,7 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
--- Pipeline test: scp without trailing slash (fix nested dirs)
+-- Pipeline test: rsync to avoid nested dirs
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
