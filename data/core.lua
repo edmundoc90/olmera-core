f6f2b5bddaf80394632ee1fbfd12b24656b1f7b5
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 09:22:04 -0600

    test: script-only change to verify workflow conditional execution

--- data/core.lua
+++ data/core.lua
@@ -5,3 +5,4 @@ dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
 -- test change
+-- Test: script-only change with new environment names
