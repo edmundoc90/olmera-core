636928aff4a276a6f2b0d95f8b770893bc38af40
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 12:30:55 -0600

    test: retrigger QA Stage 3A after enabling variables (scp path v3)

--- data/core.lua
+++ data/core.lua
@@ -1,7 +1,7 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
--- Pipeline test: scp path v2
+-- Pipeline test: scp path v3
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
