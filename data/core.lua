64fb4c5801811d1cac82054b874b01dbbb2e5dda
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 12:51:28 -0600

    test: verify QA env-based gating and scp to current/

--- data/core.lua
+++ data/core.lua
@@ -1,7 +1,7 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
--- Pipeline test: scp path v3
+-- Pipeline test: verify QA env-based gating and scp to current/
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
