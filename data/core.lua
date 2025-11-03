2a4660e1daa1a937d4b4ef1162b8299331501d49
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 11:36:07 -0600

    test: verify SSH known_hosts in workflow

--- data/core.lua
+++ data/core.lua
@@ -1,6 +1,7 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
+-- Test: SSH known_hosts verification
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
