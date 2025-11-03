69d3fe2ba97b66684e51fd2ef35782358775b949
Edmundo <castellanosedmundo@gmail.com>
2025-11-03 13:02:06 -0600

    test: verify scp without trailing slash fixes nested directories

--- data/core.lua
+++ data/core.lua
@@ -1,7 +1,7 @@
 DATA_DIRECTORY = configManager.getString(configKeys.DATA_DIRECTORY)
 CORE_DIRECTORY = configManager.getString(configKeys.CORE_DIRECTORY)
 
--- Pipeline test: verify QA env-based gating and scp to current/
+-- Pipeline test: scp without trailing slash (fix nested dirs)
 dofile(CORE_DIRECTORY .. "/global.lua")
 dofile(CORE_DIRECTORY .. "/libs/libs.lua")
 dofile(CORE_DIRECTORY .. "/stages.lua")
